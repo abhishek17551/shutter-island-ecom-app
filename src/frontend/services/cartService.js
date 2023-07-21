5543bd8464eed68265928ff80a6cd0fcb8a7bf2b
abhishek17551 <ababhishek113@gmail.com>
2023-07-21 13:20:39 +0530

    feat:defined product,wishlist,cart services

---
+++ src/frontend/services/cartService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const getCartService = async (encodedToken) => 
+    await axios.get("/api/user/cart", 
+        {headers: { authorization: encodedToken }}
+    );
+
+const addToCartService = async (post,encodedToken) => 
+    await axios.post(
+        "/api/user/cart",
+        { product },
+        {headers: { authorization: encodedToken }}
+    );
+
+const removeFromCartService = async (productId,encodedToken) => 
+        await axios.delete(
+            `api/user/cart/${productId}`,
+            {headers: { authorization: encodedToken }}
+        );
+
+const updateQuantityService = async (productId,type,encodedToken) => 
+        await axios.post(
+            `/api/user/cart/${productId}`,
+            {action: { type } },
+            {headers: { authorization: encodedToken }}
+        );
+
+export {
+    updateQuantityService,
+    removeFromCartService,
+    addToCartService,
+    getCartService
+}
